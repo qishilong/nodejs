8fb334f8f9bfe198fd85125d0d1365fec9a70f92
qishilong <663253062@qq.com>
2023-02-24 21:21:40 +0800

    1-02.全局对象

---
+++ 1-02.全局对象/src/index.js
@@ -0,0 +1,21 @@
+// const buffer = Buffer.from("abcdefg", "utf-8")
+// console.log(buffer)
+
+// console.log("当前命令行：", process.cwd())
+
+console.log(__dirname)
+// setTimeout(() => {
+//     // process.exit()
+//     console.log("abc")
+//     // process.exit()
+// }, 1000)
+
+// process.exit()
+
+// console.log(process.argv)
+
+console.log(process.platform)
+
+// process.kill(6572)
+
+console.log(process.env)
