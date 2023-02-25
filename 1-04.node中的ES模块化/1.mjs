73f6ff3349964cb72a74e289008d78630e91b9c6
qishilong <663253062@qq.com>
2023-02-25 14:28:18 +0800

    1-04.node中的模块化细节
    1-04.node中的模块化细节

---
+++ 1-04.node中的ES模块化/1.mjs
@@ -0,0 +1,6 @@
+import fs from "node:fs";
+// console.log(fs);
+
+export const a = 1;
+const b = 2
+export default b;
---
+++ 1-04.node中的ES模块化/2.mjs
@@ -0,0 +1,4 @@
+import b, { a } from "./1.mjs"
+// import b from "./1.mjs"
+// const a = require("./1")
+console.log(a, b)
